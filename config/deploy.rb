ba0b035acfeb7712807d54c40b56f4dbbe9fc1cf
riiiisa <lolepop.ri.pig.k@gmail.com>
2020-05-02 15:50:37 +0900

    デプロイ作業自動化

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ lock '3.13.0'
 
 set :application, 'chat-space'
 
-set :repo_url,  'git@github.com:riiiisa/master.git'
+set :repo_url,  'git@github.com:riiiisa/chat-space.git'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
@@ -11,7 +11,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' 
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/riiisa.pem>'] 
+                  keys: ['~/.ssh/riiisa.pem'] 
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
